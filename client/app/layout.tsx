77a00bf715c000d4dca9fb50a011b54891e2e8e3
Akshat Shukla <56386613+Nemesis-AS@users.noreply.github.com>
2025-10-22 00:21:45 +0530

    Initial PWA Setup

---
+++ client/app/layout.tsx
@@ -0,0 +1,70 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+// import LoadWS from "./loadws";
+const APP_NAME = "Predine";
+const APP_DEFAULT_TITLE = "Predine";
+const APP_TITLE_TEMPLATE = "%s - Predine";
+const APP_DESCRIPTION = "Predine - Order food without the hassle";
+
+export const metadata: Metadata = {
+  applicationName: APP_NAME,
+  title: {
+    default: APP_DEFAULT_TITLE,
+    template: APP_TITLE_TEMPLATE,
+  },
+  description: APP_DESCRIPTION,
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: "default",
+    title: APP_DEFAULT_TITLE,
+    // startUpImage: [],
+  },
+  formatDetection: {
+    telephone: false,
+  },
+  openGraph: {
+    type: "website",
+    siteName: APP_NAME,
+    title: {
+      default: APP_DEFAULT_TITLE,
+      template: APP_TITLE_TEMPLATE,
+    },
+    description: APP_DESCRIPTION,
+  },
+  twitter: {
+    card: "summary",
+    title: {
+      default: APP_DEFAULT_TITLE,
+      template: APP_TITLE_TEMPLATE,
+    },
+    description: APP_DESCRIPTION,
+  },
+};
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ client/next.config.ts
@@ -0,0 +1,13 @@
+import withSerwistInit from "@serwist/next";
+
+const isDev = process.env.NODE_ENV === "development";
+
+const withSerwist = withSerwistInit({
+  swSrc: "app/sw.ts",
+  swDest: "public/sw.js",
+  cacheOnNavigation: true,
+  disable: isDev,
+  additionalPrecacheEntries: [{ url: "/offline", revision: "1" }],
+});
+
+export default withSerwist({});
