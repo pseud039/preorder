2087b2f52c6735fa1eeeee9039d52521e82265b5
Akshat Shukla <56386613+Nemesis-AS@users.noreply.github.com>
2026-01-13 00:21:40 +0530

    Migration fixed

--- client/proxy.ts
+++ client/proxy.ts
@@ -1,6 +1,6 @@
 import { NextResponse, NextRequest } from "next/server";
 
-export default function middleware(request: NextRequest) {
+export default function proxy(request: NextRequest) {
   const reqUrl = new URL(request.url);
 
   // These routes would redirect to /dashboard if the user is authenticated
